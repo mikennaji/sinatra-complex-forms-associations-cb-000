ae8601efa4ac26d20234f792dca9cca1c398f780
Michael Nnaji <mnnaji3@uwo.ca>
2021-04-04 22:11:06 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -11,7 +11,10 @@ class PetsController < ApplicationController
   end
 
   post '/pets' do
-
+    @pet = Pet.create(params[:pet])
+    if !params["owner"]["name"].empty?
+  @pet.owner << Owner.create(name: params["owner"]["name"])
+end
     redirect to "pets/#{@pet.id}"
   end
 
@@ -21,7 +24,16 @@ class PetsController < ApplicationController
   end
 
   patch '/pets/:id' do
+    if !params[:pet].keys.include?("owner_ids")
+      params[:pet]["owner_ids"] = []
+      end
+      #######
 
+      @pet = Pet.find(params[:id])
+      @pet.update(params["pet"])
+      if !params["owner"]["name"].empty?
+        @pet.owner << Owner.create(name: params["owner"]["name"])
+      end
     redirect to "pets/#{@pet.id}"
   end
 end
