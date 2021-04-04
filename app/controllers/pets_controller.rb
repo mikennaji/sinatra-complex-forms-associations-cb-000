843c776aa5469a8d5bfaee1aa455f26f1e2b3511
Michael Nnaji <mnnaji3@uwo.ca>
2021-04-04 23:11:55 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -13,7 +13,7 @@ class PetsController < ApplicationController
   post '/pets' do
     @pet = Pet.create(params[:pet])
     if !params["owner"]["name"].empty?
-        @pet.owner = Owner.create(name: params["owner"]["name"])
+        @pet.owner = Owner.create(params[:owner])
       end
     redirect to "pets/#{@pet.id}"
   end
@@ -39,7 +39,7 @@ class PetsController < ApplicationController
       @pet = Pet.find(params[:id])
       @pet.update(params["pet"])
       if !params["owner"]["name"].empty?
-      @pet.owner = Owner.create(name: params["owner"]["name"])
+      @pet.owner = Owner.create(params[:owner])
     end
     redirect to "pets/#{@pet.id}"
   end
